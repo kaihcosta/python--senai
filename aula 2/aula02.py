212ec1bfaabfea120e95ec0cd7c3e7d85dd228bc
kaihcosta <kailaineroberto2015@gmail.com>
2024-12-03 11:32:32 -0300

    Add files via upload

---
+++ aula 2/aula02.py
@@ -0,0 +1,13 @@
+print ('ola mundo estou gripada')
+print('estou com sono')
+print('meu nome é Kailaine')
+nome_usuario='kaihcosta' #tipo->str=string
+idade_usuario=17 #17 primaveras
+altura_usuario= 1.53 # minuscula 
+maior_idade= True # mentira
+print ('seja bem vindo, nome_usuario')
+print ('sua idade é', idade_usuario, 'anos')
+print ('você tem', altura_usuario, "metros.")
+print ('voocê é maior de idade?', maior_idade)
+
+       
